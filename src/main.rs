d160e7ee585949cfe2cacfe3e1cfc68132145b10
Joe Jones <joeknockando@googlemail.com>
2017-09-15 07:54:13 +0100

    Imported time_calc. Using nightly rust. Removed input port.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,37 @@
-//! Creates a jack midi input and output ports. The application prints
-//! out all values sent to it through the input port. It also sends a
-//! Note On and Off event, once every cycle, on the output port.
+#![feature(try_from)]
+
 extern crate jack;
+extern crate time_calc;
+
+use std::convert::TryFrom;
+use std::f64;
+use std::u32;
 use std::io;
-use jack::prelude::{AsyncClient, Client, JackControl, MidiInPort, MidiInSpec, MidiOutPort,
+use jack::prelude::{AsyncClient, Client, JackControl, MidiOutPort,
                     MidiOutSpec, ClosureProcessHandler, ProcessScope, RawMidi, client_options};
+use time_calc::{
+    SampleHz, Bpm, Beats,
+};
 
 fn main() {
     // open client
-    let (client, _status) = Client::new("rust_jack_show_midi", client_options::NO_START_SERVER)
+    let (client, _status) = Client::new("kick-drum", client_options::NO_START_SERVER)
         .unwrap();
 
-    println!("The sample rate is {}", client.sample_rate());
+    // the try_from used here needs the nightly build.
+    let sample_frequency : SampleHz = f64::from( u32::try_from(client.sample_rate())
+                                            .map_err( |err| format!("Sample rate could not be converted to a u32! The error was {}", err.to_string())).unwrap());
     
-    // process logic
-    let mut maker = client.register_port("rust_midi_maker", MidiOutSpec::default()).unwrap();
-    let shower = client.register_port("rust_midi_shower", MidiInSpec::default()).unwrap();
+    let bpm: Bpm = 120.0;
+
+    let samples_per_beat = Beats(1).samples(bpm, sample_frequency);
+    println!("Samples per beat: {}", samples_per_beat);
+    
+    // make a midi port
+    let mut maker = client.register_port("midi_out", MidiOutSpec::default()).unwrap();
+    
     let cback = move |_: &Client, ps: &ProcessScope| -> JackControl {
-        let show_p = MidiInPort::new(&shower, ps);
-        for e in show_p.iter() {
-            println!("{:?}", e);
-        }
+        
         let mut put_p = MidiOutPort::new(&mut maker, ps);
         put_p.write(&RawMidi {
                 time: 0,
