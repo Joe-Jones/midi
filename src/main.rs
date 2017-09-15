01cdc4cc29bd3e55eb8710e80e16d41a8bd5d97a
Joe Jones <joeknockando@googlemail.com>
2017-09-15 16:54:15 +0100

    Kick drum 120 times a minute.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::f64;
 use std::u32;
 use std::io;
 use jack::prelude::{AsyncClient, Client, JackControl, MidiOutPort,
-                    MidiOutSpec, ClosureProcessHandler, ProcessScope, RawMidi, client_options};
+                    MidiOutSpec, ClosureProcessHandler, ProcessScope, RawMidi, client_options, JackFrames};
 use time_calc::{
     SampleHz, Bpm, Beats,
 };
@@ -24,27 +24,30 @@ fn main() {
     
     let bpm: Bpm = 120.0;
 
-    let samples_per_beat = Beats(1).samples(bpm, sample_frequency);
+    let samples_per_beat : JackFrames = u32::try_from(Beats(1).samples(bpm, sample_frequency))
+        .map_err( |err| format!("Samples per beat could not be fitted into a u32! the error was {}", err.to_string())).unwrap();
     println!("Samples per beat: {}", samples_per_beat);
     
     // make a midi port
     let mut maker = client.register_port("midi_out", MidiOutSpec::default()).unwrap();
+
+    let mut next_beat : JackFrames = 0;
     
     let cback = move |_: &Client, ps: &ProcessScope| -> JackControl {
         
         let mut put_p = MidiOutPort::new(&mut maker, ps);
-        put_p.write(&RawMidi {
-                time: 0,
+
+        while next_beat < ps.n_frames() {
+            put_p.write(&RawMidi {
+                time: next_beat,
                 bytes: &[0b10010000 /* Note On, channel 1 */,
-                         0b00100000 /* Key number */, 0b01111111 /* Velocity */],
+                         36 /* Key number */, 0b01111111 /* Velocity */],
             })
-            .unwrap();
-        put_p.write(&RawMidi {
-                time: ps.n_frames() / 2,
-                bytes: &[0b10000000 /* Note Off, channel 1 */,
-                         0b00100000 /* Key number */, 0b01111111 /* Velocity */],
-            })
-            .unwrap();
+                .unwrap();
+            next_beat += samples_per_beat;
+        }
+        next_beat -= ps.n_frames();
+                
         JackControl::Continue
     };
 
